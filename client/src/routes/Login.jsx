2b13105fa96494841bacc61864766de8ebda072c
valeiras <d.r.valeiras@gmail.com>
2023-10-20 10:03:53 +0200

    Adds automatic build script

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
   return (
     <Wrapper>
       <Form method="post" className="form">
-        <h4>Inicia sesión</h4>
+        <h4>Iniciar sesión</h4>
         <LoginFormRow name="email" type="email" />
         <LoginFormRow name="password" type="password" />
 
--- server.js
+++ server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, './client/dist')));
 app.use(cookieParser());
 app.use(express.json());
 
@@ -61,7 +61,7 @@ app.use(
 );
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public/index.html'));
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
 });
 
 app.use('*', (req, res) => {
